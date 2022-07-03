256eb93a45cf7cc9769636c065f6d994a6d3471e
Илья Воронков <voronkovid@gmail.com>
2022-07-03 15:39:12 +0300

    кщдуы фтв зукьшыышщты

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -18,7 +18,7 @@ class IsAuthorOrReadOnlyPermission(permissions.BasePermission):
 class IsAdminOrReadOnlyPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-            request.user.is_authenticated and request.user.is_staff
+            request.user.is_authenticated and request.user.is_admin
             or request.method in permissions.SAFE_METHODS
         )
 
